62b58eb5d4d06ae95d3267af4cceb469c550d2f2
thiago alves <thiago87gsa@gmail.com>
2025-01-26 13:20:48 -0300

    :sparkles: feat: translate home about

--- src/components/home/AboutHome.tsx
+++ src/components/home/AboutHome.tsx
@@ -2,19 +2,21 @@ import { BiConversation } from "react-icons/bi";
 import { GrGroup } from "react-icons/gr";
 import { MdOutlineReportProblem } from "react-icons/md";
 import Button from "../Button";
+import { useTranslations } from "next-intl"; // declare this import
+
 const AboutHome = () => {
+  const t = useTranslations("Home");
   return (
     <div className="dark:bg-colorBg3Dark bg-colorBg3Light flex flex-col items-center w-full min-h-screen  pt-16 md:pt-16 dark:text-colorTextDark text-colorTextLight pb-8">
       <div className="flex flex-col gap-8 max-w-[1204px] px-4 lg:px-0">
         <div className="mb-20 ">
           <div className="flex  items-start mb-4 text-5xl font-semibold w-full max-w-[1204px]">
             <h2 className="w-full sm:w-2/4  pb-1 border-b-4 dark:border-colorHighlightsDark border-colorHighlightsLight">
-              Sobre mim
+              {t('aboutMe')}
             </h2>
           </div>
           <p className=" mx-auto md:mx-0 text-center md:text-left w-[90%] sm:w-[80%] md:w-2/5 text-lg">
-            Minha paixão por tecnologia me motiva a explorar, aprender e criar
-            soluções que impactem positivamente o mundo.
+            {t('myPassion')}
           </p>
         </div>
         <div className="flex flex-col md:flex-row gap-10 md:gap-0 items-center mb-20">
@@ -24,10 +26,9 @@ const AboutHome = () => {
                 <BiConversation className="dark:bg-colorHighlightsDark bg-colorHighlightsLight text-white p-2 rounded-full" />
               </div>
               <div>
-                <h2 className="text-2xl font-semibold mb-2">Comunicação</h2>
+                <h2 className="text-2xl font-semibold mb-2">{t('communication')}</h2>
                 <p className="w-2/3">
-                  Habilidade de transmitir ideias de forma clara e eficaz,
-                  promovendo entendimento e colaboração em projetos.
+                  {t('communicationText')}
                 </p>
               </div>
             </div>
@@ -38,11 +39,10 @@ const AboutHome = () => {
               </div>
               <div>
                 <h2 className="text-2xl font-semibold mb-2">
-                  Trabalho em equipe
+                  {t('teamwork')}
                 </h2>
                 <p className="w-2/3">
-                  Capacidade de cooperar com diferentes perfis profissionais,
-                  garantindo alinhamento e sucesso nos objetivos comuns.
+                  {t('teamworkText')}
                 </p>
               </div>
             </div>
@@ -53,11 +53,10 @@ const AboutHome = () => {
               </div>
               <div>
                 <h2 className="text-2xl font-semibold mb-2">
-                  Resolução de problemas
+                  {t('troubleshooting')}
                 </h2>
                 <p className="w-2/3">
-                  Aptidão para identificar desafios, propor soluções práticas e
-                  implementar melhorias rapidamente e com eficiência.
+                  {t('troubleshootingText')}
                 </p>
               </div>
             </div>
@@ -65,43 +64,30 @@ const AboutHome = () => {
           </div>
           <div className="flex flex-col gap-8 w-[90%] sm:w-[80%] md:w-[65%] text-center md:text-left md:pl-20 text-lg">
             <p>
-              Minhas <span className="font-bold">principais habilidades</span>{" "}
-              estão ligadas à minha capacidade de colaborar e me adaptar a
-              diferentes cenários.{" "}
-              <span className="font-bold">Comunicação</span> eficaz,{" "}
-              <span className="font-bold">trabalho em equipe</span> e{" "}
-              <span className="font-bold">resolução de problemas</span> são{" "}
-              pilares da minha atuação, permitindo que eu me conecte com as
-              pessoas e enfrente desafios com criatividade e eficiência. Além
-              disso, sou <span className="font-bold">dedicado</span>, tenho{" "}
-              <span className="font-bold">facilidade em aprender</span> novas
-              tecnologias e metodologias, e valorizo a{" "}
-              <span className="font-bold">organização</span> e a{" "}
-              <span className="font-bold">proatividade</span> para alcançar
-              resultados consistentes e de qualidade.
+              {t('my')} <span className="font-bold">{t('mainSkills')}</span>{" "}
+              {t('areLinked')}{" "}
+              <span className="font-bold">{t('effectiveCommunication')}</span> {" "}
+              <span className="font-bold">{t('teamwork')}</span> {t('and')}{" "}
+              <span className="font-bold">{t('troubleshooting')}</span> {t('arePillars')} <span className="font-bold">{t('dedicated')}</span> {t('iHaveAn')}{" "}
+              <span className="font-bold">{t('easyTimeLearning')}</span> {t('newTechnologies')}{" "}
+              <span className="font-bold">{t('organization')}</span> {t('and')} {" "}
+              <span className="font-bold">{t('proactivity')}</span> {t('toAchieve')}
             </p>
             <p>
-              No <span className="font-bold">aspecto técnico</span>, desenvolvo
-              aplicações modernas e responsivas, combinando uma experiência
-              sólida em{" "}
+              {t('onThe')} <span className="font-bold">{t('technicalSide')}</span>, {t('iDevelop')}{" "}
               <span className="font-bold">
-                HTML, CSS, JavaScript, TypeScript, React, Next.js, Redux Toolkit
-                e Tailwind
+                HTML, CSS, JavaScript, TypeScript, React, Next.js, Redux Toolkit {" "}
+                {t('and')} Tailwind
               </span>{" "}
-              no frontend, com{" "}
-              <span className="font-bold">Python, Django e MySQL</span> no
-              backend. Essa combinação de tecnologias me permite criar soluções
-              completas e integradas, sempre priorizando eficiência,
-              escalabilidade e a melhor experiência para os usuários.
+              {t('onThe')} frontend, {t('with')}{" "}
+              <span className="font-bold">Python, Django e MySQL</span> {t('onThe')} {" "}
+              backend. {t('thisCombination')}
             </p>
             <p>
-              Estou sempre em busca de novos aprendizados e desafios que me
-              permitam crescer tanto no âmbito profissional quanto no pessoal.
-              Com essa união de habilidades técnicas e comportamentais, busco
-              contribuir para projetos inovadores e que gerem impacto positivo.
+              {t('iAmAlways')}
             </p>
             <div className="flex mx-auto md:mx-0 w-[190px]">
-              <Button title="Mais" />
+              <Button title={t('more')} />
             </div>
           </div>
         </div>
--- src/components/projects/ProjectsProject.tsx
+++ src/components/projects/ProjectsProject.tsx
@@ -23,12 +23,12 @@ const ProjectsProject = () => {
 
   return (
     <div className="dark:bg-colorBg2Dark bg-colorBgLight flex flex-col justify-center items-center w-full min-h-screen pt-20  dark:text-colorTextDark text-colorTextLight overflow-x-hidden">
-      <div className="flex px-4 lg:px-0 items-start  mb-20 w-screen h-72 dark:bg-colorBg3Dark">
+      <div className="flex px-4 lg:px-0 items-start  mb-20 w-screen h-[420px] sm:h-72 bg-colorBg3Light dark:bg-colorBg3Dark">
         <div className="flex flex-col gap-8 items-center justify-center h-full px-4 lg:px-0 mb-20  w-full mx-auto  max-w-[1204px]">
-          <h2 className="text-5xl font-semibold">
+          <h2 className="text-4xl sm:text-5xl font-semibold text-center">
             Minha Jornada em Linhas de Código
           </h2>
-          <p className="text-center text-lg max-w-[680px]">
+          <p className="text-center text-base sm:text-lg max-w-[680px]">
             Cada projeto é um marco dessa trajetória, refletindo desafios
             superados, aprendizados conquistados e soluções inovadoras. Aqui
             estão os frutos do meu trabalho: aplicações que aliam
